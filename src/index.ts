f697de1afc04c16a4f0e1de4aa0dd7bab872301f
Srijan Nayak <srijannayak0@gmail.com>
2021-12-14 16:41:09 +0530

    feat: Manage API routes with a separate router

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import routes from "./routes/api";
 
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send("At root endpoint");
 });
+
+app.use("/api", routes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
