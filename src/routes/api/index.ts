5d077c5dc2ad60d22d3d5769159f47f63caa70e8
Srijan Nayak <srijannayak0@gmail.com>
2021-12-15 10:01:41 +0530

    feat: Validate query parameters for resize endpoint

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import { validateResizeQueryParameters } from "../../utilities/resize";
 
 const routes = Router();
 
 routes.get("/", (req, res) => {
   res.send("At API root endpoint");
+});
+
+routes.get("/resize", validateResizeQueryParameters, async (req, res) => {
+  res.send("Resized image");
 });
 
 export default routes;
--- src/utilities/resize.ts
+++ src/utilities/resize.ts
@@ -1,3 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { existsSync } from "fs";
+
+export const validateResizeQueryParameters = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { image, width, height } = req.query;
+  if (image === undefined || width === undefined || height === undefined) {
+    res.status(404).send("Error: Missing required query parameters");
+  } else if (!existsSync(`images/${image}`)) {
+    res.status(404).send(`Error: ${image} not found in images/`);
+  } else if (+width <= 0 || +height <= 0 || isNaN(+width) || isNaN(+height)) {
+    res.status(404).send("Error: invalid resizing dimensions");
+  } else {
+    next();
+  }
+};
+
 const writeResizedImage = async (
   imageName: string,
   width: number,
