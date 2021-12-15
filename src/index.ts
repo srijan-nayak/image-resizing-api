6e0626b41448eadc6918a4e5a3cecaf54daf6af4
Srijan Nayak <srijannayak0@gmail.com>
2021-12-15 16:52:25 +0530

    feat: Create images and thumbnails directories if they don't exist

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import express from "express";
 import routes from "./routes/api";
+import { existsSync, mkdirSync } from "fs";
 
 const app = express();
 const PORT = 3000;
+
+if (!existsSync("images")) {
+  mkdirSync("images");
+}
+
+if (!existsSync("thumbnails")) {
+  mkdirSync("thumbnails");
+}
 
 app.get("/", (req, res) => {
   res.send("At root endpoint");
